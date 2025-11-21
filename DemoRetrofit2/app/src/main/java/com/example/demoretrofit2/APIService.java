e016fb954cc28d24efa78cfe3694e8734111adbe
gbao261205 <ngbao261205@gmail.com>
2025-11-21 14:47:17 +0700

    Retrofit 2 baitap05

---
+++ DemoRetrofit2/app/src/main/java/com/example/demoretrofit2/APIService.java
@@ -0,0 +1,13 @@
+package com.example.demoretrofit2;
+
+import com.example.demoretrofit2.Model.Category;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APIService {
+    @GET("categories.php")
+    Call<List<Category>> getCategoryAll();
+}
---
+++ DemoRetrofit2/app/src/main/java/com/example/demoretrofit2/RetrofitActivity.java
@@ -0,0 +1,62 @@
+package com.example.demoretrofit2;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.demoretrofit2.Adapter.CategoryAdapter;
+import com.example.demoretrofit2.Model.Category;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RetrofitActivity extends AppCompatActivity {
+    RecyclerView rcCate;
+
+    CategoryAdapter categoryAdapter;
+    APIService apiService;
+    List<Category> categoryList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrofit);
+        AnhXa();
+        GetCategory();
+    }
+    private void AnhXa() {
+        rcCate = findViewById(R.id.rc_category);
+    }
+
+    private void GetCategory() {
+        apiService = RetrofitClient.getRetrofit().create(APIService.class);
+        apiService.getCategoryAll().enqueue(new Callback<List<Category>>() {
+            @Override
+            public void onResponse(Call<List<Category>> call, Response<List<Category>> response) {
+                if (response.isSuccessful()) {
+                    categoryList = response.body();
+
+                    categoryAdapter = new CategoryAdapter(RetrofitActivity.this, categoryList);
+                    rcCate.setHasFixedSize(true);
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+                    rcCate.setLayoutManager(layoutManager);
+                    rcCate.setAdapter(categoryAdapter);
+                    categoryAdapter.notifyDataSetChanged();
+                } else {
+                    int statusCode = response.code();
+
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Category>> call, Throwable t) {
+                Log.d("logg",t.getMessage());
+            }
+        });
+    }
+}
